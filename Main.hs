d6372d398077293c7ee7e5a60feb140986bad252
Andrey Chudnov <oss@chudnov.com>
2014-12-07 20:37:24 -0500

    Test

---
+++ Main.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Language.ECMAScript5.Parser
+import Language.ECMAScript5.Syntax
+import Data.ByteString
+import Data.String
+import Prelude hiding (getContents, putStr)
+
+main :: IO ()
+main = do src <- getContents
+          case parse program "" src of
+           Left err -> putStr $ "Error: " `append` (fromString $ show err)
+           Right js -> putStr "Parse successful"
+          
